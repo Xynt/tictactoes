f3aa659247969d7d4aa8e901531128c98e2e0f81
xaro <xabilro@gmail.com>
2023-05-09 14:10:18 +0200

    do more things

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { GameFieldComponent } from './game-field/game-field.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GameFieldComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
