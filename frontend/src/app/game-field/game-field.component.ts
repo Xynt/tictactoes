08aaa638b7e0eb7369703a4b6caf87e1e7d4cde8
xaro <xabilro@gmail.com>
2023-05-10 09:42:05 +0200

    add fancy x's and o's

--- frontend/src/app/game-field/game-field.component.ts
+++ frontend/src/app/game-field/game-field.component.ts
@@ -20,9 +20,36 @@ export class GameFieldComponent implements OnInit {
     Player.NONE,
   ]
 
+  public currentTurn: Player = Player.ONE;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  clickedBox(i: number): void {
+    if (this.fields[i] === Player.NONE) {
+      this.fields[i] = this.currentTurn;
+      this.toggleTurn();
+    }
+  }
+
+  isX(i: number): boolean {
+    return this.fields[i] === Player.ONE;
+  }
+
+  isO(i: number): boolean {
+    return this.fields[i] === Player.TWO;
+  }
+
+  isEmpty(i: number) {
+    return this.fields[i] === Player.NONE;
+  }
+
+  private toggleTurn(): void {
+    if (this.currentTurn === Player.ONE) {
+      this.currentTurn = Player.TWO;
+    } else {
+      this.currentTurn = Player.ONE;
+    }
+  }
 }
