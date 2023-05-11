2f90bd6c91a21d27e4e4bd83896c82bfe7887d6a
xaro <xabilro@gmail.com>
2023-05-11 09:54:43 +0200

    add initial screen

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,12 +6,14 @@ import {GameFieldComponent} from './game-field/game-field.component';
 import {ChatComponent} from './chat/chat.component';
 import {FormsModule} from "@angular/forms";
 import {WebsocketService} from "./services/websocket.service";
+import { InitialScreenComponent } from './initial-screen/initial-screen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameFieldComponent,
     ChatComponent,
+    InitialScreenComponent,
   ],
   imports: [
     BrowserModule,
